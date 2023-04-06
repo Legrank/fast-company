3fd80387f51bad6984fdb0d4a812bfe994a6b9be
legrank <flyu.gyu@yandex.ru>
2023-04-06 19:37:45 +0300

    add fireBase

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import useMockData from '../hooks/mockData'
 
-export default function Main() {
-    return <div>Main</div>
+const Main = () => {
+    const { error, initialize, progress, status } = useMockData()
+    const handleClick = () => {
+        initialize()
+    }
+    return (
+        <div className="container mt-5">
+            <h1> Main Page</h1>
+            <h3>Инициализация данных в FireBase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>Error: {error}</li>}
+            </ul>
+            <button className="btn btn-primary" onClick={handleClick}>
+                Инициализировать
+            </button>
+        </div>
+    )
 }
+
+export default Main
